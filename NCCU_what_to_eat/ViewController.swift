6f1333c5ba9f90380fce72ad87caa4658c513a7b
Huang, Yi-Tang <ty80517@gmail.com>
2024-07-23 09:50:07 +0800

    Initial Commit

---
+++ NCCU_what_to_eat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NCCU_what_to_eat
+//
+//  Created by 黃翊唐 on 2024/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
